6f2f6b6f110a621574f4d87b6666f883d6f7c884
IvanKavaliov <ivan.kavaliov@gmail.com>
2022-08-23 20:14:26 +0300

    feat: add template for admin panel

--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -17,5 +17,11 @@ Route::group(['namespace' => '\App\Http\Controllers\Main'], function () {
     Route::get('/', IndexController::class);
 });
 
+Route::group(['namespace' => '\App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', IndexController::class);
+    });
+});
+
 Auth::routes();
 
