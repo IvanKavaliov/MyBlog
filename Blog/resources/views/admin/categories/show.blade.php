06ea0fd5f19f5b4ddf3e56503ab8d6940d486911
IvanKavaliov <ivan.kavaliov@gmail.com>
2023-03-29 23:49:30 +0300

    feat: implement get one category

--- Blog/resources/views/admin/categories/show.blade.php
+++ Blog/resources/views/admin/categories/show.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Categories</h1>
+                    <h1 class="m-0">{{ $category->title }}</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -23,32 +23,20 @@
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
-            <div class="row">
-                <div class="col-1 mb-3">
-                    <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Add</a>
-                </div>
-            </div>
             <div class="row">
                 <div class="col-6">
                     <div class="card">
                         <div class="card-body table-responsive p-0">
                             <table class="table table-hover text-nowrap">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Title</th>
-                                    <th>View</th>
-                                </tr>
-                                </thead>
                                 <tbody>
-                                @foreach($categories as $category)
                                 <tr>
+                                    <td>Id</td>
                                     <td>{{ $category->id }}</td>
-                                    <td>{{ $category->title }}</td>
-                                    <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="far fa-eye"></i></a></td>
                                 </tr>
-                                @endforeach
+                                <tr>
+                                    <td>Title</td>
+                                    <td>{{ $category->title }}</td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
