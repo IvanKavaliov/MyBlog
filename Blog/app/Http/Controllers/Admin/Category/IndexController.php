1141fbcb292a1caa41eb6e69af421fba6180386c
IvanKavaliov <ivan.kavaliov@gmail.com>
2023-03-29 23:19:28 +0300

    feat: implement read categories

--- Blog/app/Http/Controllers/Admin/Category/IndexController.php
+++ Blog/app/Http/Controllers/Admin/Category/IndexController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
+        $categories = Category::all();
         // TODO: Implement __invoke() method.
-        return view('admin.categories.index');
+        return view('admin.categories.index', compact('categories'));
     }
 }
