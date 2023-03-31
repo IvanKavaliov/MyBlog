076790245f76c8ea8c475524c51ddfd57a445219
IvanKavaliov <ivan.kavaliov@gmail.com>
2023-03-31 17:07:34 +0300

    feat: add atribute for download images

--- Blog/app/Http/Controllers/Admin/Post/StoreController.php
+++ Blog/app/Http/Controllers/Admin/Post/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
@@ -12,6 +13,9 @@ class StoreController extends Controller
     {
         // TODO: Implement __invoke() method.
         $data = $request->validated();
+        $data['preview_image'] = Storage::put('/image', $data['preview_image']);
+        $data['main_image'] = Storage::put('/image', $data['main_image']);
+
         Post::firstOrCreate($data);
 
         return redirect()->route('admin.post.index');
--- Blog/resources/views/admin/layouts/main.blade.php
+++ Blog/resources/views/admin/layouts/main.blade.php
@@ -67,6 +67,7 @@
 </script>
 <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
 <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+<script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
 <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 <script>
@@ -82,6 +83,9 @@
                 ['height', ['height']]
             ]
         });
+    });
+    $(function () {
+        bsCustomFileInput.init();
     });
 </script>
 </body>
