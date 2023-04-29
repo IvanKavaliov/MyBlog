379ad3121749b1d349b0edfea5529a0f39c62042
IvanKavaliov <ivan.kavaliov@gmail.com>
2023-04-29 15:31:40 +0300

    feat: add class Job

--- Blog/app/Models/User.php
+++ Blog/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Notifications\SendVerifyWithQueueNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
